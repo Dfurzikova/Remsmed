cba3391150a61970d0615689db9f61eb2aed559e
Dfurzikova <dfurzikova@yandex.ru>
2019-02-01 16:06:05 +0300

    Добавила global data

--- page/src/components/App.js
+++ page/src/components/App.js
@@ -1,10 +1,54 @@
 import React, { Component } from 'react';
 import './App.scss';
 import '../fonts/fonts.scss'
-
 import ScreenGreeting from './ScreenGreeting/ScreenGreeting'
 import ScreenHealthDiary from './ScreenHealthDiary/ScreenHealthDiary'
 import AppFunctions from './AppFunctions/AppFunctions'
+import ScreenPersonalDiary from './ScreenPersonalDiary/ScreenPersonalDiary'
+
+
+const globalData = {
+    appFunctions: [
+        {
+            title: 'Планирование графика\nприема лекарств',
+            icon: require('../assets/function-icon_1.png')
+        },
+        {
+            title: 'Уведомление врача об исполнении назначений',
+            icon: require('../assets/function-icon_2.png')
+        },
+        {
+            title: 'Ведение дневника самочувствия',
+            icon: require('../assets/function-icon_3.png')
+        },
+        {
+            title: 'Удаленное предоставление измерений врачу',
+            icon: require('../assets/function-icon_4.png')
+        },
+        {
+            title: 'Общение с врачами через встроенный мессенджер',
+            icon: require('../assets/function-icon_5.png')
+        },
+        {
+            title: 'Получение уведомлений для выполнения назначений',
+            icon: require('../assets/function-icon_6.png')
+        }
+    ],
+    personalDiaryReasons: [
+        {
+            title: 'Хранение данных',
+            text: 'Контролируйте и следите за динамикой изменения показателей вашего здоровья. Вносите результаты измерений медицинских показателей вручную или с помощью интегрированных приборов, храните историю измерений и отслеживайте динамику состояния здоровья'
+        },
+        {
+            title: 'Сообшение информации врачу',
+            text: 'Позвольте специалисту дистанционно следить за вашим здоровьем. Будьте на связи со специалистом - отправляйте информацию о своем самочувствии, обменивайтесь сообщениями и получайте рекомендации по вашему здоровью и лечению.'
+        },
+        {
+            title: 'Напоминание о важном',
+            text: 'Получайте рекомендации, созданные специально для вас. Система, персонально настроенная под ваши требования, автоматически напомнит о необходимости выполнить рекомендации медицинского специалиста: принять препарат или выполнить измерения.'
+        }
+    ]
+};
 
 class App extends Component {
     render() {
@@ -12,7 +56,8 @@ class App extends Component {
             <div className="App">
                 <ScreenGreeting />
                 <ScreenHealthDiary />
-                <AppFunctions />
+                <AppFunctions functions={globalData.appFunctions} />
+                <ScreenPersonalDiary reasons={globalData.personalDiaryReasons}/>
             </div>
         );
     }
