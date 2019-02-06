08eef825ee3886c0bfd9763175a42601edadb819
Dfurzikova <dfurzikova@yandex.ru>
2019-02-06 20:27:07 +0300

    Добавила build

---
+++ build/precache-manifest.4a9dac3abd900049ff9e0964ea0ecc17.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "89c05bffd2317100db3767839eed006b",
+    "url": "/static/media/phones.89c05bff.png"
+  },
+  {
+    "revision": "3b9296b3c99f091cb0ea",
+    "url": "/static/css/main.f03715a1.chunk.css"
+  },
+  {
+    "revision": "531039200bdb72d760ad",
+    "url": "/static/js/1.53103920.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4fca61df8d93f64cf79fd4dc833bce1b",
+    "url": "/static/media/CeraRoundPro-Bold.4fca61df.otf"
+  },
+  {
+    "revision": "ef283a0703f4243113c3a318978ba09b",
+    "url": "/static/media/CeraRoundPro-Regular.ef283a07.otf"
+  },
+  {
+    "revision": "d8ab35a62595177faf38ca532f8fe3e3",
+    "url": "/static/media/Geometria.d8ab35a6.otf"
+  },
+  {
+    "revision": "3b9296b3c99f091cb0ea",
+    "url": "/static/js/main.3b9296b3.chunk.js"
+  },
+  {
+    "revision": "a37f30cadd5af0e1412ecd484c7199d0",
+    "url": "/static/media/lines.a37f30ca.png"
+  },
+  {
+    "revision": "c91e8d9fcb0f18252e30e8bb5a3798b1",
+    "url": "/static/media/analytics.c91e8d9f.png"
+  },
+  {
+    "revision": "f00711713fe79de8d62ad27cdb071214",
+    "url": "/static/media/header-image_1.f0071171.jpg"
+  },
+  {
+    "revision": "91106bc3fb69c17518e75f532eb47bed",
+    "url": "/static/media/background-lines.91106bc3.jpg"
+  },
+  {
+    "revision": "5dd12e45207cafebe7520fa1e048b05f",
+    "url": "/static/media/mob.5dd12e45.png"
+  },
+  {
+    "revision": "0343d1b954f7446edb570d43efbdd1b7",
+    "url": "/static/media/graph.0343d1b9.png"
+  },
+  {
+    "revision": "488a9d218cfa1fe949f8a0f34b01b5ee",
+    "url": "/index.html"
+  }
+];
